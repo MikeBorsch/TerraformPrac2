633f359d62428eb3aea388deb5c10a470fe03fa2
MikeBorsch <michaelcborsch@gmail.com>
2025-04-19 15:24:50 -0400

    New Main

---
+++ main.tf
@@ -0,0 +1,48 @@
+terraform {
+  backend "remote" {
+    organization = "TerraformPractice-mike"
+    workspaces {
+      name = "Example-Workspace"
+    }
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" { #platform
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "the-sky-spoopy-bucket"
+
+  tags = {
+    Name = "TheAwesomeCatBucket"
+  }
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-014d544cfef21b42d"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ShilohsInstance"
+  }
+}
+
+resource "aws_instance" "app_server2" {
+  ami           = "ami-014d544cfef21b42d"
+  instance_type = "t2.micro"
+
+
+tags = {
+    Name = "TofusInstance"
+  }
+}
+
